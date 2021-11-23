490815348f28b79b9ac31678ea63ef400d9b25c3
Sreerekha  k v <sreerekhakv86@gmail.com>
2021-11-23 11:08:59 +0530

    Date changes to date

--- lib/forms/transaction_forms/chick_transferEditor.dart
+++ lib/forms/transaction_forms/chick_transferEditor.dart
@@ -121,7 +121,7 @@ class _ChickTransferState extends State<ChickTransfer> {
                             child: TextFormField(
                               decoration: InputDecoration(
                                 border: InputBorder.none,
-                                hintText: 'Date',
+                                hintText: 'date',
                                 hintStyle: TextStyle(
                                     fontWeight: FontWeight.bold,
                                     color: Colors.black38),
